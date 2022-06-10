76da9db63e8f25a2f6ee8215d2bdb9d788d08423
huynhlan158 <huynhlan158@gmail.com>
2022-06-10 11:40:42 +0700

    Khắc Phục Vấn Đề Nhiều File Index.js

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home page</h2>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+function Home() {
+  return <div>Home page</div>;
+}
+
+export default Home;
